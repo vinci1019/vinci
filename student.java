beb8e41b04459c0e82d2aeb0c373247b21d37e19
vinci1019 <vincijoseph10@gmail.com>
2021-02-25 12:34:48 +0300

    https://github.com/vinci1019/vinci.git

---
+++ student.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+    public class student {
+        public static void main(String[] args) {
+        String[] letters = {"A"};
+        for (String i:letters) {
+            System.out.println(i);
+//A for add
+            Scanner myObj = new Scanner (System.in);
+            System.out.println("Please enter the student name:TOM, Please enter the course you want to add:comp2011,Adding course:comp2011");
+String name = myObj.nextLine();
+String course = myObj.nextLine();
+System.out.println("student name:"+ name);
+System.out.println("course name:" + course);
+
+  
+
+
+
+
+        }
+        }
+    }
+
